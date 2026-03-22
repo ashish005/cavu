0c66b34156617c47255f3842d5d79f9e3d7c0563
ashish005 <me.ashish005@gmail.com>
2026-03-22 12:06:58 -0700

    changes

--- src/portals/company/views/home.view.ts
+++ src/portals/company/views/home.view.ts
@@ -1,21 +1,25 @@
 import {Component, OnInit, TemplateRef} from '@angular/core';
-import { AppSetupService, AppSetup } from "@app-global";
+import {AppSetup, AppSetupService, TypingComponent} from "@app-global";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
-import {AuthService} from "@app-third-party";
 
 @Component({
     templateUrl: './templates/home.html',
+  styleUrls: [`./templates/home.css`],
   standalone: true,
   imports: [
     CommonModule, // Add CommonModule to imports
-    FormsModule, RouterModule
+    FormsModule, RouterModule, TypingComponent
   ]
 })
 export class HomeView implements OnInit {
   app: AppSetup;
-  constructor(private setupService: AppSetupService, private authService: AuthService){}
+  typingOptions = {
+    period: 800,
+    info: [ 'IT Consulting Services', 'Digital Solution Provider' ]
+  };
+  constructor(private setupService: AppSetupService){}
   ngOnInit() { this.app = this.setupService.appSetup; }
   onActivate(componentRef) { }
 }
---
+++ src/portals/company/views/vision/visionComponent.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: './vision.html',
+    styleUrls: [`./new-pricing.css`]
+})
+export class VisionComponent {
+}
